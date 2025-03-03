fdb95a7a1a0d01605c34c2e9f6b9333ca577bba8
ravi10690 <raviahuja1998@gmail.com>
2025-03-04 00:10:39 +0530

    adding the promts and examples

--- agent.py
+++ agent.py
@@ -1,12 +1,14 @@
 # Create the graph + all nodes
+import loading_env
 from langgraph.graph import StateGraph, START, END, MessagesState
 from functions import task_mAIstro, update_todos, update_profile, update_instructions, route_message
-from langgraph.store.base import BaseStore
 from langgraph.store.memory import InMemoryStore
 from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.messages import HumanMessage
 
 
 
+# Create the graphss
 builder = StateGraph(MessagesState)
 
 # Define the flow of the memory extraction process
@@ -14,7 +16,7 @@ builder.add_node(task_mAIstro)
 builder.add_node(update_todos)
 builder.add_node(update_profile)
 builder.add_node(update_instructions)
-builder.add_edge(START, "task_mAIstro")
+builder.add_edge(START, "task_mAIstro") 
 builder.add_conditional_edges("task_mAIstro", route_message)
 builder.add_edge("update_todos", "task_mAIstro")
 builder.add_edge("update_profile", "task_mAIstro")
@@ -28,3 +30,54 @@ within_thread_memory = MemorySaver()
 
 # We compile the graph with the checkpointer and store
 graph = builder.compile(checkpointer=within_thread_memory, store=across_thread_memory)
+
+
+config = {"configurable": {"thread_id": "1", "user_id": "Lance"}}
+
+# User input to create a profile memory
+input_messages = [HumanMessage(content="My name is Lance. I live in india with my wife. I have a 1 year old daughter.")]
+
+for chunk in graph.stream({"messages": input_messages}, config, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
+    
+
+
+input_messages = [HumanMessage(content="My wife asked me to book swim lessons for the baby.")]
+
+# Run the graph
+for chunk in graph.stream({"messages": input_messages}, config, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
+
+
+
+# User input to update instructions for creating ToDos
+input_messages = [HumanMessage(content="When creating or updating ToDo items, include specific local businesses / vendors.")]
+
+# Run the graph
+for chunk in graph.stream({"messages": input_messages}, config, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
+
+
+# User input for a ToDo
+input_messages = [HumanMessage(content="I need to fix the jammed electric Yale lock on the door.")]
+
+# Run the graph
+for chunk in graph.stream({"messages": input_messages}, config, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
+
+
+# User input to update an existing ToDo
+input_messages = [HumanMessage(content="For the swim lessons, I need to get that done by end of November.")]
+
+# Run the graph
+for chunk in graph.stream({"messages": input_messages}, config, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
+
+config = {"configurable": {"thread_id": "2", "user_id": "Lance"}}
+
+# Chat with the chatbot
+input_messages = [HumanMessage(content="I have 30 minutes, what tasks can I get done?")]
+
+# Run the graph
+for chunk in graph.stream({"messages": input_messages}, config, stream_mode="values"):
+    chunk["messages"][-1].pretty_print()
